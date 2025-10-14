0aedd36bbfb6cf21bfe0e63b0ac4f4edeed3a401
Boules Emad <boulesemad16@gmail.com>
2025-10-14 16:28:26 +0300

    queries to generate data & generated data added

---
+++ generating data/fill intake.sql
@@ -0,0 +1,11 @@
+INSERT INTO intake (intakename, startdate) VALUES
+('intake 45', '2025-01-01'),
+('intake 46', '2025-05-01'),
+('intake 47', '2025-09-01'),
+('intake 48', '2026-01-01'),
+('intake 49', '2026-05-01'),
+('intake 50', '2026-09-01'),
+('intake 51', '2027-01-01'),
+('intake 52', '2027-05-01'),
+('intake 53', '2027-09-01'),
+('intake 54', '2028-01-01');
